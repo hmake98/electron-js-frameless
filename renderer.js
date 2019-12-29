a0d6e67ef3fee1491df7cf9294cd1563506a20e4
Harsh Makwana <harsh.make1998A@gmail.com>
2019-12-29 06:24:58 +0530

    Added tray and window.

--- renderer.js
+++ renderer.js
@@ -1,6 +1,10 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const ipc = require('electron').ipcRenderer;
+const userOS = navigator.appVersion.indexOf('Mac') !== -1 ? 'mac' : 'win';
+
+document.getElementById('tray_option_window').classList.add(userOS);
+
+document.getElementById('quit').addEventListener('click', () => {
+    console.log("Hey")
+    ipc.send('quit', true);
+});
+
